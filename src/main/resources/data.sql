d98f7e8d09ed16249c29abee0d2dd4cf0c6fe37d
Андрей <andrekukshinov@gmail.com>
2020-07-06 20:14:14 +0300

    add database schema and data on countries

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO countries(country_name)
+VALUES ('Belarus'),('Ukraine'),('USA'),('Japan'),('Canada');
+# INSERT INTO rooms(room_name, country_id)
+# VALUES ('first room', 1),
+#        ('second room', 1),
+#        ('third room', 1),
+#        ('fourth room', 2),
+#        ('fifth room', 2),
+#        ('sixth room', 2),
+#        ('seventh room', 3),
+#        ('eighth room', 3),
+#        ('ninth room', 3);
+# INSERT INTO lamps(room_id)
+# VALUES (1),(2),(3),(4),(5),(6),(7),(8),(9);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -21,20 +21,5 @@ CREATE TABLE IF NOT EXISTS lamps
 
 );
 
-INSERT INTO countries(country_name)
-VALUES ('Belarus'),('Ukraine'),('USA');
-INSERT INTO rooms(room_name, country_id)
-VALUES ('first room', 1),
-       ('second room', 1),
-       ('third room', 1),
-       ('fourth room', 2),
-       ('fifth room', 2),
-       ('sixth room', 2),
-       ('seventh room', 3),
-       ('eighth room', 3),
-       ('ninth room', 3);
-INSERT INTO lamps(room_id)
-VALUES (1),(2),(3),(4),(5),(6),(7),(8),(9);
-
 
 
